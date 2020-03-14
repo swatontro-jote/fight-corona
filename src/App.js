01f6f80364649d1c5ddb7de32dde479b52b3bf59
r00t <algocerebro@gmail.com>
2020-03-15 00:37:33 +0600

    yo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +17,41 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <span className="headd">
+          <img className="logoo"
+            src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/p960x960/53246902_2547024495346950_2987768474471759872_o.jpg?_nc_cat=107&_nc_sid=85a577&_nc_eui2=AeFQ2Yn-HYUCuVp9R1f2fGffkLTblcOV6JC2-2Ne1feEQgIy4pBdkBn2EEESn65iu61PqlGdyynEFbVu6yS5wiqlMAGoCtIYWLOal01z0MDUnQ&_nc_ohc=_uJR3gEyDhgAX-ff4dd&_nc_ht=scontent.fdac5-1.fna&_nc_tp=6&oh=564095c2efe470586cf23ed3f6321beb&oe=5E938517"
+          >
+          </img>
+          <p className="likha"><strong>#করোনার_সময়_ক্লাস_নয়</strong> <br/> <a href="https://www.facebook.com/events/220022776049501/">ইভেন্টে</a> প্ল্যাকার্ড হাতে নিজের ছবি পোষ্ট করুন এবং আমাদের পাশে এসে দাঁড়ান।  </p>
+        </span>
+        <div className="stand">
+          <img 
+          className = "images"
+          src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/s960x960/89959275_10158244502566462_3525709133976174592_o.jpg?_nc_cat=104&_nc_sid=02d79f&_nc_eui2=AeHTdLjtSXm4hCsNJlL57Pf6ic3JEDcUK99-eeBNPkHXC9enRa3HTwSYx3cihwIl9-pJkmuy8fgvIQI1z4mz2D5_4P41yO5aeaO_EujPUEGIKA&_nc_ohc=wsxvOuLa7IwAX_nJnbk&_nc_ht=scontent.fdac5-1.fna&_nc_tp=7&oh=482dcd25dfca9e5a7c0b32ce1fc1b835&oe=5E917392"></img>
+
+          <img 
+          className = "images"
+          src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/s960x960/89517715_3279631145419611_6734181310476058624_o.jpg?_nc_cat=106&_nc_sid=02d79f&_nc_eui2=AeEaoCOLbRnPjwBMx63YGhGnmvEFaSwMyts5Lna4MieYFR5wLbrXuMNrqd_7d8JSinuyuxMGja043gBAuokZ6iP5W_C7R_TXBxLLRz96oVoi5g&_nc_ohc=d0Sw4SHsgrEAX8jyaAx&_nc_ht=scontent.fdac5-1.fna&_nc_tp=7&oh=e3165edc2aa96ffb8bcd1ea0f2afad62&oe=5E912B3E"></img>
+          <img 
+          className = "images"
+          src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/p720x720/90085617_2330000553965550_5473272233419866112_o.jpg?_nc_cat=107&_nc_sid=0be424&_nc_eui2=AeEMDrklEM8dy8tjnVTJ-nQ1HDAeUe5IF2LRnFfGUOiCz8JFRkwXxTXjDGaApLITNDmwGmrEBG-m29OdXtP6YTNsOV2npQRUE2LJYcm73e2dVQ&_nc_ohc=USVf2hHVfgMAX-X1CSd&_nc_ht=scontent.fdac5-1.fna&_nc_tp=6&oh=24ad94c2d18dbc40d798850243967da8&oe=5E909901"></img>
+          <img
+          className="images"
+          src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/s960x960/89808031_2691401454439067_3647147856801824768_o.jpg?_nc_cat=106&_nc_sid=02d79f&_nc_eui2=AeF5FktVuAXnLcRFPZecrLcDGGJhLRlgeyMUn18OwBO-7u7BN0-JuFMndu_iTeq5HRNyPY-rrstDyDa-9Vss71GiOkR-LeqIiO7gaKwJ2BYOTA&_nc_ohc=o5-Z-NR12m8AX9D16jl&_nc_ht=scontent.fdac5-1.fna&_nc_tp=7&oh=69271909d5884eed2e530c8f4aea3329&oe=5E912071"></img>
+          <img 
+          className="images"
+          src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/s960x960/89549836_1307943652927248_8470623295325077504_o.jpg?_nc_cat=109&_nc_sid=02d79f&_nc_eui2=AeEcFJrLoPZHsdLZCuhKeHXT8AMGnqGktTeCuIOatqTHkcvxuxiG7IZ34IplD0aoXSNXqOoAEQde-i7G5C5APReqMwp9nt_03s2Up50jRIVrPA&_nc_ohc=9lbpY_PzPaQAX-UGJrT&_nc_ht=scontent.fdac5-1.fna&_nc_tp=7&oh=a465bb2b255750be359463796a6aa676&oe=5E915209"></img> 
+          <img 
+          className="images"
+          src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/s960x960/89992938_1118447678499343_5652855368800272384_o.jpg?_nc_cat=108&_nc_sid=02d79f&_nc_eui2=AeHvCPEAaBeFymr53_nrLzaMZZ43UNhKvmKHaFUOHOFlgLYLAwbR7tOx_-Z1doAFQVGRGv1FBpZpvLKTmWQn8kNGBqOjLODOEoDRbsblJXPMcg&_nc_ohc=4Yp_OVPpKeoAX_5G8Pv&_nc_ht=scontent.fdac5-1.fna&_nc_tp=7&oh=7231fcfbd5cad4132ffab23caba6bc7c&oe=5E94769C"></img>
+          <img 
+          className="images"
+          src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/s960x960/89722063_2543136045955838_6531744568214290432_o.jpg?_nc_cat=102&_nc_sid=02d79f&_nc_eui2=AeG_PUpM6nFbI0jv96OtApL6rJn5VlP0hw97EjP5-FOI-mbjJFhiu1BKUyeJMy6sDOm2kRctR9NjYhHX5eplaZPNyMdM4pY9oGLwNIed8_FCvQ&_nc_ohc=fQe4-kc3WiwAX9C3dL7&_nc_ht=scontent.fdac5-1.fna&_nc_tp=7&oh=55b9aaf07adea36ff0efe528f3ba7e39&oe=5E91652A"></img>
+          <img 
+          className="images"
+          src="https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/s960x960/90081713_1535561309927788_5286378788823760896_o.jpg?_nc_cat=106&_nc_sid=110474&_nc_ohc=VGyJ7o9X0QMAX_U1a75&_nc_ht=scontent.fdac5-1.fna&_nc_tp=7&oh=bab68be35a023950bb6b813e031a3b68&oe=5E944A15"></img>
+          </div>
       </header>
     </div>
   );
