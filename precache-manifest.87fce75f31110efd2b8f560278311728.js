4776b7847c6fac5cf46dcd414cc9e79ad0ddc834
r00t <algocerebro@gmail.com>
2020-03-15 00:40:56 +0600

    Updates

---
+++ precache-manifest.87fce75f31110efd2b8f560278311728.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20df8c7d377d1c75d4a85a5d673b8452",
+    "url": "/fight-corona/index.html"
+  },
+  {
+    "revision": "b1d1fb8562f2cc6d4e1b",
+    "url": "/fight-corona/static/css/main.c2abcfd9.chunk.css"
+  },
+  {
+    "revision": "740549a7580c9e8c43c6",
+    "url": "/fight-corona/static/js/2.a9f28692.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/fight-corona/static/js/2.a9f28692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d1fb8562f2cc6d4e1b",
+    "url": "/fight-corona/static/js/main.a3c4417d.chunk.js"
+  },
+  {
+    "revision": "5012aa97cdaedbc641cf",
+    "url": "/fight-corona/static/js/runtime-main.aa3d5221.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fight-corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
